9b284ee42fcfab90be6d9eb30bf2d28af4916027
DongWendao <2521632021@qq.com>
2021-03-03 17:22:37 +0800

    完成了客户端的CRUD

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '../views/Login.vue'
+import Welcome from "../views/Welcome";
 import System_Home_Consume from "../views/System_Home_Consume";
+//引入订单
 import AddOrders from "../views/Orders/AddOrders";
 import SelectOrders from "../views/Orders/SelectOrders";
 import UpdateOrders from "../views/Orders/UpdateOrders";
-import Welcome from "../views/Welcome";
+import SelectOrdersById from "../views/Orders/SelectOrdersById";
+//引入客户
+import AddCustomers from "../views/customers/AddCustomers";
+import SelectCustomers from "../views/customers/SelectCustomers";
+import UpdateCustomers from "../views/customers/UpdateCustomers";
 
 Vue.use(VueRouter)
 
@@ -56,6 +62,36 @@ const routes = [
         path: '/system_home_consume/updateOrders',
         name: '修改订单',
         component: UpdateOrders
+      },
+      {
+        path: '/system_home_consume/selectorderbyid',
+        name: 'id查询订单',
+        component: SelectOrdersById
+      }
+    ]
+  },
+  //2  订单管理
+  {
+    path: '/system_home_consume',
+    name: '客户管理',
+    component: System_Home_Consume,
+    //redirect: "/system_home_consume/selectCustomers",
+    show: true,
+    children:[
+      {
+        path: '/system_home_consume/selectCustomers',
+        name: '查询客户',
+        component: SelectCustomers
+      },
+      {
+        path: '/system_home_consume/addCustomers',
+        name: '新增客户',
+        component: AddCustomers
+      },
+      {
+        path: '/system_home_consume/updateCustomers',
+        name: '修改客户',
+        component: UpdateCustomers
       }
     ]
   }
